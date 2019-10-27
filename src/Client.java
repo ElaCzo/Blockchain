ba560a17192cdd09a9d0366b2e06b76259ce9837
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2019-10-27 14:51:15 +0100

    Thread de lecture des commandes.

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,7 @@
+import Pair2Pair.Message;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -7,6 +11,9 @@ import net.i2p.crypto.eddsa.EdDSAPublicKey;
 import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
+import java.util.*;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.nio.CharBuffer;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -22,13 +29,44 @@ import java.util.Scanner;
 
 
 public class Client {
-
     protected Socket socketOfClient;
     protected DataOutputStream os;
     protected DataInputStream is;
-    
-    public Client (String serverHost, int port) throws JSONException, UnknownHostException, IOException, NoSuchAlgorithmException, NoSuchProviderException {
 
+    protected int tour;
+
+    ArrayList<Lettre> lettres;
+
+    public void readingInChanel(){
+        Thread recevoir = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String msg;
+                try {
+                    do {
+                        msg = Util.readMsg(is);
+                        if (Messages.isNextTurn(msg)) {
+                            tour = Messages.nextTurn(msg);
+                        }
+                        else{
+                            System.out.println("Commande serveur non reconnue.");
+                        }
+                    }while(true);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Serveur déconnecté");
+                try {
+                    closeConnection();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        recevoir.start();
+    }
+
+    public Client (String serverHost, int port) throws JSONException, UnknownHostException, IOException, NoSuchAlgorithmException, NoSuchProviderException {
         // Send a request to connect to the server is listening
         // on machine 'localhost' port 9999.
         socketOfClient = new Socket(serverHost, port);
@@ -38,16 +76,19 @@ public class Client {
 
         // Input stream at Client (Receive data from the server).
         is = new DataInputStream(socketOfClient.getInputStream());
-    	
+
+        lettres = new ArrayList<Lettre>();
+
+        readingInChanel();
     }
-    
-    
+
+
     public void closeConnection() throws IOException {
     	os.close();
     	is.close();
     	socketOfClient.close();
     }
-    
+
     public static void main(String[] args) throws UnknownHostException, JSONException, IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
 
         if(args.length!=2) {
@@ -58,8 +99,7 @@ public class Client {
         final String serverHost = args[0];
         final int port = Integer.valueOf(args[1]);
         Client c = new Client(serverHost, port);
-        
+
 
     }
-
 }
