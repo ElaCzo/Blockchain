cceda2cc315418047edb8f505397682f461c0d5d
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2019-10-27 14:56:31 +0100

    Classe Block implémentée.

--- src/Block.java
+++ src/Block.java
@@ -3,11 +3,61 @@ import java.util.ArrayList;
 public class Block {
     private String mot;
     private String hash;
-    private Block suivant;
+    private int AuteurID;
+    private int nonce;
 
-    ArrayList<Lettre> lettres = new ArrayList<>();
+    ArrayList<Lettre> lettres;
 
-    public Block(){
+    public Block(String mot, ArrayList<Lettre> lettres, int AuteurID, int nonce) {
+        this.mot = mot;
+        this.lettres = new ArrayList<>(lettres);
+        this.AuteurID = AuteurID;
+        this.nonce = nonce;
+        hash();
+    }
 
+
+    /**
+     * @return the hash
+     */
+    public String getHash() {
+        return hash;
+    }
+
+    /**
+     * @return the lettres
+     */
+    public ArrayList<Lettre> getLettres() {
+        return lettres;
+    }
+
+    /**
+     * @return the mot
+     */
+    public String getMot() {
+        return mot;
+    }
+
+    /**
+     * @return the auteutID
+     */
+    public int getAuteurID() {
+        return AuteurID;
+    }
+
+    /**
+     * @return the nonce
+     */
+    public int getNonce() {
+        return nonce;
+    }
+
+    public void hash() {
+        try {
+            byte[] nhash = Sha.hash_sha256(new String(mot + "" + AuteurID + "" + nonce));
+            this.hash = Util.bytesToString(nhash);
+        } catch (Exception e) {
+            System.out.println("Erreur SHA dans LETTRE");
+        }
     }
 }
